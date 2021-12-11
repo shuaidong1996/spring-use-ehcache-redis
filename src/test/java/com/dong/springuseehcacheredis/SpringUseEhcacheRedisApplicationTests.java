caf47b62a231ed48abd61df573ab4f6fe4cecedb
shuaidong1996 <438003405@qq.com>
2021-12-11 15:56:12 +0800

    enable ehcache

---
+++ src/test/java/com/dong/springuseehcacheredis/SpringUseEhcacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.springuseehcacheredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUseEhcacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
