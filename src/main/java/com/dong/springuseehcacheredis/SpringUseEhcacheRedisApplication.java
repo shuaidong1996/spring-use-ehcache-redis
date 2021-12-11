e4b66af3e793d13c22621c463f2417990adfa45a
shuaidong1996 <438003405@qq.com>
2021-12-11 15:55:12 +0800

    enable ehcache

---
+++ src/main/java/com/dong/springuseehcacheredis/SpringUseEhcacheRedisApplication.java
@@ -0,0 +1,15 @@
+package com.dong.springuseehcacheredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(locations = "classpath:cache/cache_manager.xml")
+public class SpringUseEhcacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUseEhcacheRedisApplication.class, args);
+    }
+
+}
